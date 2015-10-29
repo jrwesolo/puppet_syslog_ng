f2b3e7e1f2b22ab19a9e74d3f9d49ca655a8f295
Jordan Wesolowski <jrwesolo@gmail.com>
2015-10-29 15:47:11 -0700

    removing unnecessary code

--- manifests/destination.pp
+++ manifests/destination.pp
@@ -15,28 +15,3 @@ define syslog_ng::destination (
   }
 
 }
-
-# # syslog network destination
-# define syslog_ng::destination::network (
-#   $log_server = undef,
-#   $log_port   = undef,
-#   $proto      = 'udp',
-#   ) {
-#   case $proto {
-#     'UDP', 'udp': {
-#       syslog_ng::destination {$title:
-#         spec   => "udp('${log_server}' port(${log_port}) log_fifo_size(${::syslog_ng::log_fifo_size_destination}));",
-#         target => $syslog_ng::params::config_file_destination_remote
-#       }
-#     }
-#     'TCP', 'tcp': {
-#       syslog_ng::destination {$title:
-#         spec   => "tcp('${log_server}' port(${log_port}) log_fifo_size(${::syslog_ng::log_fifo_size_destination}));",
-#         target => $syslog_ng::params::config_file_destination_remote
-#       }
-#     }
-#     default: {
-#       fail("${proto} is not supported by syslog_ng::client")
-#     }
-#   }
-# }
