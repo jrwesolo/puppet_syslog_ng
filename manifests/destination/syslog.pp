90df68ed08b0051dde5ede2969beabc789148fac
Jordan Wesolowski <jrwesolo@gmail.com>
2015-11-04 11:46:46 -0700

    add defines for syslog source and destination

---
+++ manifests/destination/syslog.pp
@@ -0,0 +1,30 @@
+# syslog syslog destination
+define syslog_ng::destination::syslog (
+  $host            = undef,
+  $comment         = undef,
+  $flags           = undef,
+  $flush_lines     = undef,
+  $identifier      = $title,
+  $ip_protocol     = undef,
+  $localip         = undef,
+  $localport       = undef,
+  $log_fifo_size   = undef,
+  $network         = undef,
+  $order           = undef,
+  $port            = undef,
+  $suppress        = undef,
+  $template        = undef,
+  $template_escape = undef,
+  $time_zone       = undef,
+  $transport       = undef,
+) {
+
+  syslog_ng::destination { $title:
+    spec       => template('syslog_ng/destination/syslog.erb'),
+    identifier => $identifier,
+    comment    => $comment,
+    order      => $order,
+  }
+
+}
+
---
+++ manifests/source/syslog.pp
@@ -0,0 +1,28 @@
+# syslog syslog source
+define syslog_ng::source::syslog (
+  $comment          = undef,
+  $flags            = undef,
+  $host_override    = undef,
+  $identifier       = $title,
+  $ip_protocol      = undef,
+  $keep_hostname    = undef,
+  $keep_timestamp   = undef,
+  $localip          = undef,
+  $localport        = undef,
+  $max_connections  = undef,
+  $order            = undef,
+  $program_override = undef,
+  $tags             = undef,
+  $time_zone        = undef,
+  $transport        = undef,
+) {
+
+  syslog_ng::source { $title:
+    spec       => template('syslog_ng/source/syslog.erb'),
+    identifier => $identifier,
+    comment    => $comment,
+    order      => $order,
+  }
+
+}
+
